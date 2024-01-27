e063f5ee81f828d81cb2752a91e589d498829557
Igor Kolpakov <kolpakovigorjob@gragass.ru>
2024-01-27 14:54:05 +0300

    refactor: routing on page.ts file in pages instead of importing directly, eager

---
+++ src/pages/auth/onboarding/page.ts
@@ -0,0 +1,7 @@
+import { OnBoardingPage } from "@/pages/auth/onboarding/ui/OnBoardingPage.tsx";
+import { currentRoute } from "@/pages/auth/onboarding/model";
+
+export default {
+  view: OnBoardingPage,
+  route: currentRoute,
+};
